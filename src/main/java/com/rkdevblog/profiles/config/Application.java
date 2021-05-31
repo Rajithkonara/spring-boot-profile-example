33aa4f9235f285ec210e2c3292fa8b1df95f9e9e
rajithkonara <rajithkonara@gmail.com>
2021-05-31 23:30:59 +0530

    updated pom and added configuration classes

---
+++ src/main/java/com/rkdevblog/profiles/config/Application.java
@@ -0,0 +1,11 @@
+package com.rkdevblog.profiles.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Application {
+  private String welcomeMessage;
+  private String XApiKey;
+}
